ce880f1f481ac390a881bf6f26833141985d6a5f
tempravikalro <soujanyameets@gmail.com>
2023-06-18 16:20:23 -0500

    Commit for Demo3 project

---
+++ src/main/java/com/kalro/demo3git/Demo3GitApplication.java
@@ -0,0 +1,14 @@
+package com.kalro.demo3git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3GitApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Demo3.");
+	}
+
+}
---
+++ src/test/java/com/kalro/demo3git/Demo3GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalro.demo3git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo3GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
